27193cfd970cfd87b4f3c5409817d8f955859ca3
Oliver Bryant <oliver@ufomail.co.uk>
2015-08-31 18:41:51 +0000

    GlazerCalc WPF
    a simple adoption of the GlazerCalc provided in the C# yellow book.

---
+++ GlazerCalc/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GlazerCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            string woodResult, glassResult;
+            widthString = tbWidth.Text;
+            heightString = tbHeight.Text;
+            if (widthString == "")
+            {
+                MessageBox.Show("Error: Width is invaild, provide a number.");
+            }
+            else
+            {
+                width = double.Parse(widthString);
+                height = double.Parse(heightString);
+                woodLength = 2 * (width * height) * 3.25;
+                woodResult = ("The length of wood is " + woodLength + " feet");
+                label.Content = woodResult;
+            }
+            if (heightString == "")
+            {
+                MessageBox.Show("Error: Widh is invaild, provide a number.");
+            }
+            else
+            {
+                height = double.Parse(heightString);
+                width = double.Parse(widthString);
+                glassArea = 2 * (width * height);
+                glassResult = ("The Area of glass is " + glassArea + " square meters");
+                label1.Content = glassResult;
+            }
+            
+            
+            
+        }
+    }
+}
